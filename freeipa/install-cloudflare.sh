c65c9d6a4965d512a63a81c906edf92bb4832098
Steve HOLWEG <skytep@gmail.com>
2019-08-12 00:08:39 +0200

    Correct typo

--- freeipa/install-cloudflare.sh
+++ freeipa/install-cloudflare.sh
@@ -8,5 +8,5 @@ mkdir -p /opt/freeipa-certbot
 read -p "Cloudflare user: " CFUSER
 read -p "Cloudflare password: " CFPASSWORD
 
-printf 'dns_cloudflare_email=${CFUSER}\ndns_cloudflare_api_key=${CFPASSWORD}\n' > /opt/freeipa-certbot/cloudflare.ini
+printf "dns_cloudflare_email=${CFUSER}\ndns_cloudflare_api_key=${CFPASSWORD}\n" > /opt/freeipa-certbot/cloudflare.ini
 chmod a-x,go-rw /opt/freeipa-certbot/cloudflare.ini
--- proxmox/install-cloudflare.sh
+++ proxmox/install-cloudflare.sh
@@ -8,5 +8,5 @@ mkdir -p /opt/proxmox-certbot
 read -p "Cloudflare user: " CFUSER
 read -p "Cloudflare password: " CFPASSWORD
 
-printf 'dns_cloudflare_email=${CFUSER}\ndns_cloudflare_api_key=${CFPASSWORD}\n' > /opt/proxmox-certbot/cloudflare.ini
+printf "dns_cloudflare_email=${CFUSER}\ndns_cloudflare_api_key=${CFPASSWORD}\n" > /opt/proxmox-certbot/cloudflare.ini
 chmod a-x,go-rw /opt/proxmox-certbot/cloudflare.ini
