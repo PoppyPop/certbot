4acfaeced9940b4c76ac415fe7279e0abca305cb
Steve HOLWEG <PoppyPop@users.noreply.github.com>
2019-11-24 19:52:54 +0100

    Update certbot-freeipa-deploy.sh

--- freeipa/certbot-freeipa-deploy.sh
+++ freeipa/certbot-freeipa-deploy.sh
@@ -3,6 +3,10 @@
 
 set -e
 
+ipa-cacert-manage install "${RENEWED_LINEAGE}/chain.pem"
+
+ipa-certupdate
+
 # Install the new Key/Cert, root does not need passwords like mere mortals
 echo '' | ipa-server-certinstall -w -d "${RENEWED_LINEAGE}/fullchain.pem" "${RENEWED_LINEAGE}/privkey.pem" --dirman-password='' --pin=''
 
